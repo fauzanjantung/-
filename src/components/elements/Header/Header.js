a9e9d3fb59d1ee6e702777ee4b454d8fd3e57db3
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-12-03 05:36:09 +0700

    Logo

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/reactMoview_logo.png" alt="rmdb-logo" />
+                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
         </div>
