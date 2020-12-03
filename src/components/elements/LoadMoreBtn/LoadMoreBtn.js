f64766d10b0b793fab13f593b6abbe41717f7199
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-12-04 06:01:42 +0700

    Components

---
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoadMoreBtn.css'
+
+const LoadMorebtn = (props) => {
+    return (
+        <div>
+            Load More
+        </div>
+    )
+}
+
+export default LoadMorebtn
