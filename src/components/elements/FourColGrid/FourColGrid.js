caa8479bed6ba95f25862b26b532967615b56c6b
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-12-09 06:51:08 +0700

    Set Local Storage

--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -1,5 +1,5 @@
-import { element } from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
 import './FourColGrid.css'
 
 const FourColGrid = (props) => {
@@ -24,6 +24,11 @@ const FourColGrid = (props) => {
             </div>
         </div>
     )
+}
+
+FourColGrid.propTypes = {
+    header: PropTypes.string,
+    loading: PropTypes.bool
 }
 
 export default FourColGrid
